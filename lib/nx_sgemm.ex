4e7c0647baf27a506948108c57bddaebcf704e3c
Susumu Yamazaki <zacky1972@gmail.com>
2024-12-08 20:32:59 +0900

    feat: implement NxSgemm.multiply when u8 tensor and u8 scalar

--- lib/nx_sgemm.ex
+++ lib/nx_sgemm.ex
@@ -27,4 +27,58 @@ defmodule NxSgemm do
 
   """
   def ok(), do: :erlang.nif_error(:not_loaded)
+
+  @doc """
+  Element-wise multiplication of two tensors.
+
+  If a number is given, it is converted to a tensor.
+
+  It will broadcast tensors whenever the dimensions do not match and broadcasting is possible.
+
+  ## Examples
+
+  ### Multiplying scalers
+
+      iex> NxSgemm.multiply(1, 2)
+      #Nx.Tensor<
+        s32
+        2
+      >
+
+  ### Multiplying tensors and scalers
+
+      iex> NxSgemm.multiply(Nx.tensor([1, 2, 3], names: [:data], type: :u8), 1)
+      #Nx.Tensor<
+        u8[data: 3]
+        [1, 2, 3]
+      >
+
+      iex> NxSgemm.multiply(1, Nx.tensor([1, 2, 3], names: [:data], type: :u8))
+      #Nx.Tensor<
+        u8[data: 3]
+        [1, 2, 3]
+      >
+  """
+  def multiply(a, b) when is_integer(a) and is_integer(b) do
+    Nx.tensor(a * b, type: :s32)
+  end
+
+  def multiply(a, b) when is_integer(b) when 0 <= b and b < 256 do
+    case Nx.type(a) do
+      {:u, 8} ->
+        %{
+          a
+          | data: %{
+            a.data
+            | state: mul_nif_u8_tensor_u8_scalar(Nx.size(a), a.data.state, b)
+          }
+        }
+    end
+  end
+
+  def multiply(a, b) when is_integer(a) when 0 <= a and a < 256 do
+    multiply(b, a)
+  end
+
+  defp mul_nif_u8_tensor_u8_scalar(_size, _a, _b), do: raise("NIF mul_nif_u8_tensor_u8_scalar/3 not implemented")
 end
--- nif_src/libnif.c
+++ nif_src/libnif.c
@@ -1,13 +1,54 @@
 #include <erl_nif.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 static ERL_NIF_TERM ok(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
     return enif_make_atom(env, "ok");
 }
 
+static ERL_NIF_TERM mul_nif_u8_tensor_u8_scalar(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    if (__builtin_expect(argc != 3, false)) {
+        return enif_make_badarg(env);
+    }
+
+    ErlNifUInt64 vec_size;
+    if (__builtin_expect(!enif_get_uint64(env, argv[0], &vec_size), false)) {
+        return enif_make_badarg(env);
+    }
+
+    ERL_NIF_TERM binary_term = argv[1];
+    ErlNifBinary in_data;
+    if (__builtin_expect(!enif_inspect_binary(env, binary_term, &in_data), false)) {
+        return enif_make_badarg(env);
+    }
+
+    ERL_NIF_TERM uint_term = argv[2];
+    unsigned int factor;
+    if (__builtin_expect(!enif_get_uint(env, uint_term, &factor), false)) {
+        return enif_make_badarg(env);
+    }
+
+    uint8_t *in = (uint8_t *)in_data.data;
+    ErlNifBinary out_data;
+    if (__builtin_expect(!enif_alloc_binary(vec_size * sizeof(uint8_t), &out_data), false)) {
+        return enif_make_badarg(env);
+    }
+
+    uint8_t *out = (uint8_t *)out_data.data;
+
+    for(ErlNifUInt64 i = 0; i < vec_size; i++) {
+        out[i] = (uint8_t) (in[i] * factor); 
+    }
+
+    return enif_make_binary(env, &out_data);
+}
+
 static ErlNifFunc nif_funcs [] =
 {
-    {"ok", 0, ok}
+    {"ok", 0, ok},
+    {"mul_nif_u8_tensor_u8_scalar", 3, mul_nif_u8_tensor_u8_scalar}
 };
 
 ERL_NIF_INIT(Elixir.NxSgemm, nif_funcs, NULL, NULL, NULL, NULL)
