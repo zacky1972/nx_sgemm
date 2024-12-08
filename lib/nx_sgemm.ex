20debdc3dfef94d4a11732b4eb83752b804fbb4b
Susumu Yamazaki <zacky1972@gmail.com>
2024-12-08 18:59:33 +0900

    initial commit

---
+++ lib/nx_sgemm.ex
@@ -0,0 +1,18 @@
+defmodule NxSgemm do
+  @moduledoc """
+  Documentation for `NxSgemm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NxSgemm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nx_sgemm_test.exs
@@ -0,0 +1,8 @@
+defmodule NxSgemmTest do
+  use ExUnit.Case
+  doctest NxSgemm
+
+  test "greets the world" do
+    assert NxSgemm.hello() == :world
+  end
+end
