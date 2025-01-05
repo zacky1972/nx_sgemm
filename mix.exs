780815328dc4bbc4a8febb685b3d497dc5ae0aad
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-06 08:59:52 +0900

    refactor: apply mix format

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NxSgemm.MixProject do
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:ex_task] ++ Mix.compilers
+      compilers: [:ex_task] ++ Mix.compilers()
     ]
   end
 
