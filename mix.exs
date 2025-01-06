41a023832fbfe101ebe7f9d1996b16bff9383c93
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-07 02:15:23 +0900

    Install and set up ex_doc (#6)
    * build: install ex_doc
    
    * build: set up ex_doc
    
    * ci: add mix docs to workflow

--- mix.exs
+++ mix.exs
@@ -8,7 +8,12 @@ defmodule NxSgemm.MixProject do
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:ex_task] ++ Mix.compilers()
+      compilers: [:ex_task] ++ Mix.compilers(),
+
+      # Docs
+      name: "NxSgemm",
+      source_url: "https://github.com/zacky1972/nx_sgemm",
+      docs: docs()
     ]
   end
 
@@ -25,8 +30,16 @@ defmodule NxSgemm.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
       {:ex_task, "~> 0.1", runtime: false},
       {:nx, "~> 0.9"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "NxSgemm",
+      extras: ["README.md", "LICENSE"]
     ]
   end
 end
